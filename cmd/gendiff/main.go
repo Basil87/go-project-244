56ff843f1098fa851a15863b6b155ee22bd4d27a
Vasiliy Aleksandrov <alex.v.a@inbox.ru>
2026-03-11 17:46:57 +0300

    step-1 add help flag

---
+++ cmd/gendiff/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/urfave/cli/v3"
+)
+
+func main() {
+	cmd := &cli.Command{
+		Name:  "difference-calculator",
+		Usage: "Compare and get diff of structs",
+
+		Action: func(ctx context.Context, cmd *cli.Command) error {
+			fmt.Println("Oh, Hi Mark!")
+			return nil
+		},
+	}
+
+	cli.RootCommandHelpTemplate = `
+	./bin/gendiff --help
+	NAME:
+	   gendiff - Compares two configuration files and shows a difference.
+	
+	USAGE:
+	   gendiff [global options]
+	
+	GLOBAL OPTIONS:
+	   --help, -h                  show help
+`
+
+	if err := cmd.Run(context.Background(), os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
